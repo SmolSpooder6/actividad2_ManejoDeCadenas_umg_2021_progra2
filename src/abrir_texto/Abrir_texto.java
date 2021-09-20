bf15f6e6d05b2fbcce39bca78743ca8133f95aa0
SmolSpooder6 <gcastillo00@gmail.com>
2021-09-19 22:58:34 -0600

    first_commit

---
+++ src/abrir_texto/Abrir_texto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abrir_texto;
+
+/**
+ *
+ * @author gerar
+ */
+public class Abrir_texto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_abrir_txt frm = new frm_abrir_txt();
+        frm.setVisible(true);
+    }
+    
+}
